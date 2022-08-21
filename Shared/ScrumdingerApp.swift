05314be8a745c86e86a94b335c19ea2e1dcda1e2
Quy Do Ngoc <quydn720@gmail.com>
2022-08-21 08:06:23 +0700

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Quy Do Ngoc on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
